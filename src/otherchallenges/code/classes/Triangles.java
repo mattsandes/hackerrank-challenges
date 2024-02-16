e445596ccd53175e840cb21c8f9207550fc26008
Mateus Sandes <mateussandes@hotmail.com.br>
2024-02-16 02:29:44 -0300

    Classe Tiangles

---
+++ src/otherchallenges/code/classes/Triangles.java
@@ -0,0 +1,35 @@
+package otherchallenges.code.classes;
+
+public class Triangles {
+	
+	private double side1;
+	private double side2;
+	private double hypotenuse;
+	
+	public double getSide1() {
+		return side1;
+	}
+	
+	public void setSide1(double side1) {
+		this.side1 = side1;
+	}
+	
+	public double getSide2() {
+		return side2;
+	}
+	
+	public void setSide2(double side2) {
+		this.side2 = side2;
+	}
+	
+	public double getHypotenuse() {
+		return hypotenuse;
+	}
+	
+	public void setHypotenuse(double hypotenuse) {
+		this.hypotenuse = hypotenuse;
+	}
+	
+	
+
+}
